1d5caff1a77132d3474b94de05b7e785f4874eef
Pablo Rodriguez Gonzalez <76404961+Parogo72@users.noreply.github.com>
2026-03-20 12:05:42 +0100

    Add files via upload

---
+++ project_7.srcs/sources_1/imports/sources-20260313/rs232receiver.vhd
@@ -0,0 +1,140 @@
+-------------------------------------------------------------------
+--
+--  Fichero:
+--    rs232receiver.vhd  12/09/2023
+--
+--    (c) J.M. Mendias
+--    Dise�o Autom�tico de Sistemas
+--    Facultad de Inform�tica. Universidad Complutense de Madrid
+--
+--  Prop�sito:
+--    Conversor elemental de una linea serie RS-232 a paralelo con 
+--    protocolo de strobe
+--
+--  Notas de dise�o:
+--    - Parity: NONE
+--    - Num data bits: 8
+--    - Num stop bits: 1
+--
+-------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity rs232receiver is
+  generic (
+    FREQ_KHZ : natural;  -- frecuencia de operacion en KHz
+    BAUDRATE : natural   -- velocidad de comunicacion
+  );
+  port (
+    -- host side
+    clk     : in  std_logic;   -- reloj del sistema
+    rst     : in  std_logic;   -- reset s�ncrono del sistema
+    dataRdy : out std_logic;   -- se activa durante 1 ciclo cada vez que hay un nuevo dato recibido
+    data    : out std_logic_vector (7 downto 0);   -- dato recibido
+    -- RS232 side
+    RxD     : in  std_logic    -- entrada de datos serie del interfaz RS-232
+  );
+end rs232receiver;
+
+-------------------------------------------------------------------
+
+use work.common.all;
+
+architecture syn of rs232receiver is
+  
+  component synchronizer
+      generic (
+        STAGES : natural;
+        XPOL   : std_logic
+      );
+      port (
+        clk   : in  std_logic;
+        x     : in  std_logic;
+        xSync : out std_logic
+      );
+  end component;
+  
+  component rs232receiver_control
+      Port ( 
+        rst: in STD_LOGIC;
+        clk: in STD_LOGIC;
+        RxDSync: in STD_LOGIC;
+        readRxD: in STD_LOGIC;
+        bit_done : in STD_LOGIC;
+        stateCE: out STD_LOGIC;
+        baudCntCE: out STD_LOGIC;
+        dataRdy: out STD_LOGIC;
+        RxDSh: out STD_LOGIC
+      );
+  end component;
+  
+  component rs232receiver_datos
+      Port ( 
+        rst: in STD_LOGIC;
+        clk: in STD_LOGIC;
+        RxDSync: in STD_LOGIC;
+        bit_done: out STD_LOGIC;
+        data: out STD_LOGIC_VECTOR(7 downto 0);
+        stateCE: in STD_LOGIC;
+        RxDsh: in STD_LOGIC
+      );
+  end component;
+  signal RxDSync : std_logic;
+  signal readRxD, baudCntCE, bit_done, stateCE, RxDsh : std_logic;
+
+begin
+
+  RxDSynchronizer : synchronizer
+    generic map ( STAGES => 2, XPOL => '1' )
+    port map ( clk => clk, x => RxD, xSync => RxDSync );
+    
+  baudCnt:
+  process (clk)
+    constant CYCLES : natural := (FREQ_KHZ*1000)/BAUDRATE;
+    variable count  : natural range 0 to CYCLES-1 := 0;
+  begin
+    readRxD <= '1' when count = CYCLES/2-1 else '0';
+    if rising_edge(clk) then
+      if rst = '1' then
+        count := 0;
+      else 
+          if baudCntCE = '1' then
+            if count = CYCLES-1 then
+                count := 0;
+            else
+                count := count + 1;
+            end if;
+          else
+            count := 0;
+          end if;
+      end if;
+    end if;
+  end process;
+  
+  datos : rs232receiver_datos
+    port map ( 
+        clk => clk, 
+        rst => rst, 
+        RxDSync => RxDSync,
+        bit_done => bit_done,
+        data => data,
+        stateCE => stateCE,
+        RxDsh => RxDsh
+        );
+        
+   control : rs232receiver_control
+    port map ( 
+        clk => clk, 
+        rst => rst, 
+        RxDSync => RxDSync,
+        dataRdy => dataRdy,
+        readRxD => readRxD,
+        bit_done => bit_done,
+        stateCE => stateCE,
+        baudCntCE => baudCntCE,
+        RxDsh => RxDsh
+        );
+  
+  
+end syn;
